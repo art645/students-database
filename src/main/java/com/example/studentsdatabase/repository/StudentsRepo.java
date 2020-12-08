15b2e75eebf83dfada79950f1f2ff39f12dbda4f
art645 <aefanov95@inbox.ru>
2020-12-08 18:17:47 +0300

    added query method findMaxId

--- src/main/java/com/example/studentsdatabase/repository/StudentsRepo.java
+++ src/main/java/com/example/studentsdatabase/repository/StudentsRepo.java
@@ -1,7 +1,10 @@
 package com.example.studentsdatabase.repository;
 
 import com.example.studentsdatabase.model.Student;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StudentsRepo extends CrudRepository<Student,Long> {
+    @Query(value = "select max(s.id) from students s",nativeQuery = true)
+    Long findMaxId();
 }
